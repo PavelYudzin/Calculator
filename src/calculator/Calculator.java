ad4dbb2a000e9d572f1acc83cf38657f2ab7ce2c
Pavel Yudzin <pavel.yudzinn@gmail.com>
2020-12-25 21:46:02 +0300

    finished

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
-        Scene scene = new Scene(root, 354, 384);
+        Scene scene = new Scene(root, 354, 393);
         scene.getStylesheets().add(Calculator.class.getResource("style.css").toExternalForm());
         primaryStage.setTitle("CALCULATOR");
         primaryStage.setScene(scene);
